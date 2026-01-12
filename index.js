5306c54e70bac3735527a2ed3e30d7ef9318118c
Joaquin Padin Sancho <joaquinpadinsancho@MacBook-Air-de-Joaquin.local>
2026-01-12 13:17:19 +0100

    Implement routes for custom objects

--- index.js
+++ index.js
@@ -8,24 +8,65 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+require('dotenv').config();
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    // Usamos tu ID de objeto de Superheroes
+    const customObjectEndpoint = 'https://api.hubspot.com/crm/v3/objects/2-55931714?properties=name,level_of_power,superpower';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const resp = await axios.get(customObjectEndpoint, { headers });
+        const data = resp.data.results;
+        res.render('homepage', { title: 'Superheros Registry | Integrating With HubSpot I Practicum', data });      
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
+app.get('/update-cobj', (req, res) => {
+    res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
+app.post('/update-cobj', async (req, res) => {
+    const newHero = {
+        properties: {
+            "name": req.body.name,
+            "superpower": req.body.superpower,
+            "level_of_power": req.body.level_of_power
+        }
+    }
 
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
+    const createHeroEndpoint = 'https://api.hubapi.com/crm/v3/objects/2-55931714';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
 
-* * App.get sample
+    try { 
+        await axios.post(createHeroEndpoint, newHero, { headers } );
+        res.redirect('/');
+    } catch(err) {
+        console.error(err);
+    }
+});
+
+/** * * This is sample code to give you a reference for how you should structure your calls. 
+* * (El código de ejemplo original sigue aquí abajo...)
+*/
+// * App.get sample
 app.get('/contacts', async (req, res) => {
     const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
     const headers = {
@@ -41,7 +82,7 @@ app.get('/contacts', async (req, res) => {
     }
 });
 
-* * App.post sample
+// * App.post sample
 app.post('/update', async (req, res) => {
     const update = {
         properties: {
@@ -64,8 +105,6 @@ app.post('/update', async (req, res) => {
     }
 
 });
-*/
-
 
 // * Localhost
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
